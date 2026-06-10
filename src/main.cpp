aca65dd998af3bedadfdd71f405a826a442cef39
Muhammad Zendy Zainuddin <zendyzainuddin425@gmail.com>
2026-06-10 17:19:41 +0700

    Successfully opened raylib window

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,20 @@
 #include "raylib.h"
 
 int main() {
-    // Simple hello world program to test the build system
-    std::cout << "Hello, World!" << std::endl;
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(800, 600, "Resizable Window Example");
+    
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        
+        DrawRectangle(150, 150, 200, 150, Color{ 0, 121, 241, 127 });
+        DrawRectangle(100, 100, 200, 150, Color{ 230, 41, 55, 127 });
+
+        EndDrawing();
+    }
+    
+    CloseWindow();
+
     return 0;
 }
