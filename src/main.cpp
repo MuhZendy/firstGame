485994718df769186a536eaea247b70a39d16c8e
Muhammad Zendy Zainuddin <zendyzainuddin425@gmail.com>
2026-06-12 15:18:44 +0700

    Got imgui to work

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include "raylib.h"
+#include "imgui.h"
+#include "rlImGui.h"
 
 int main() {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 600, "Resizable Window Example");
     
+    rlImGuiSetup(true);
+
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         
+        rlImGuiBegin();
+
         DrawRectangle(150, 150, 200, 150, Color{ 0, 121, 241, 127 });
         DrawRectangle(100, 100, 200, 150, Color{ 230, 41, 55, 127 });
+
+        ImGui::Begin("Hello, ImGui!");
+        ImGui::Text("This is a resizable window example using raylib and ImGui.");
+        ImGui::End();
+
+        rlImGuiEnd();
 
         EndDrawing();
     }
     
+    rlImGuiShutdown();
+
     CloseWindow();
 
     return 0;
