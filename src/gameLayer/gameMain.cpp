7394c893d4584c7f65163fa389a45b331074aa65
Muhammad Zendy Zainuddin <zendyzainuddin425@gmail.com>
2026-06-17 08:21:44 +0700

    Added Asset Manager

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -1,14 +1,16 @@
+#include "assetManager.h"
 #include "raylib.h"
 #include "gameMain.h"
 
 struct GameState {
     // Add game state variables here
-    float playerX = 100.0f;
-    float playerY = 100.0f;
 } gameState;
+
+AssetManager assetManager;
 
 bool initGame() {
     // Initialize game resources here
+    assetManager.loadAll();
     return true; // Return false if initialization fails
 }
 
@@ -16,12 +18,10 @@ bool updateGame() {
     // Update game logic here
     // Return false if the game should be closed
     float deltaTime = GetFrameTime();
-    if (IsKeyDown(KEY_RIGHT)) gameState.playerX += 200.0f * deltaTime;
-    if (IsKeyDown(KEY_LEFT)) gameState.playerX -= 200.0f * deltaTime;
-    if (IsKeyDown(KEY_DOWN)) gameState.playerY += 200.0f * deltaTime;
-    if (IsKeyDown(KEY_UP)) gameState.playerY -= 200.0f * deltaTime;
+    if (deltaTime > 0.1f) deltaTime = 0.1f; // Cap deltaTime to avoid big jumps
 
-    DrawRectangle(gameState.playerX, gameState.playerY, 50, 50, Color{ 255, 255, 255, 255 });
+    DrawTexturePro(assetManager.dirt, { 0, 0, (float)assetManager.dirt.width, (float)assetManager.dirt.height }, 
+        { 50, 50, 100, 100 }, {}, 0, WHITE); // Example of drawing the dirt texture at the top-left corner
 
     return true;
 }
