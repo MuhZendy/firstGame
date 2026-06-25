fb4a0d66bc64d613ac449c77963ad742fa83c9ca
Muhammad Zendy Zainuddin <zendyzainuddin425@gmail.com>
2026-06-25 17:42:16 +0700

    Messed around with terrain generation

--- src/gameLayer/worldGenerator.cpp
+++ src/gameLayer/worldGenerator.cpp
@@ -2,27 +2,49 @@
 #include "worldGenerator.h"
 #include "randomStuff.h"
 
+static float lerp(float a, float b, float t) {
+    return a + (b - a) * t;
+}
+
+static void adjustNoise(float& noise, float power) {
+    noise = (noise + 1) / 2;
+    noise = std::pow(noise, power);
+}
+
 void generateWorld(GameMap& gameMap, int seed) {
     int width = gameMap.width;
     int height = gameMap.height;
 
-    int dirtHeight = height / 4; // Example: Half the height is dirt
-
     std::ranlux24_base rng(seed++); // Random number generator
 
-    auto dirtNoise = FastNoise::New<FastNoise::Perlin>();
-    auto stoneNoise = FastNoise::New<FastNoise::Perlin>();
+    auto planeNoise = FastNoise::New<FastNoise::Perlin>();
+    auto mountainNoise = FastNoise::New<FastNoise::Perlin>();
+    auto planeMountainMixer = FastNoise::New<FastNoise::Perlin>();
 
-    int previousHeight = dirtHeight; // Start with the initial dirt height
+    int dirtOffsetStart = -5;
+    int dirstOffsetEnd = 10;
+
+    int stoneHeightStart = height / 4;
+    int stoneHeightEnd = height;
 
     // Example: Fill the bottom half with dirt and the top half with air
     for (int x = 0; x < width; ++x) {
 
-        float fDirtHeight = dirtNoise->GenSingle2D(x, 0, seed);
-        int dirtHeight = fDirtHeight * 20 + 20;
+        float fMountainComponent = mountainNoise->GenSingle2D(x, 0, seed);
+        adjustNoise(fMountainComponent, 3);
 
-        float fStoneHeight = dirtNoise->GenSingle2D(x, 0, seed+1);
-        int stoneHeight = fStoneHeight * 20 + 30;
+        float fPlaneComponent = planeNoise->GenSingle2D(x, 0, seed+1);
+        adjustNoise(fPlaneComponent, 1);
+
+        float fPlaneMountainMix = planeMountainMixer->GenSingle2D(x, 0, seed+2);
+        adjustNoise(fPlaneMountainMix, 1);
+
+        float stoneNoise = lerp(fPlaneComponent, fMountainComponent, fPlaneMountainMix);
+        float dirtNoise = 0.5f;
+
+        int stoneHeight = lerp(stoneHeightStart, stoneHeightEnd, stoneNoise);
+        int dirtOffset = lerp(dirtOffsetStart, dirstOffsetEnd, dirtNoise);
+        int dirtHeight = stoneHeight - dirtOffset;
 
         for (int y = 0; y < height; ++y) {
             if (y == dirtHeight) {
