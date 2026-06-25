b9613f882c9b14303deb31eae8d2d7bb455d7a63
Muhammad Zendy Zainuddin <zendyzainuddin425@gmail.com>
2026-06-25 11:08:29 +0700

    Implemented basic terrain generation using noise

--- src/gameLayer/worldGenerator.cpp
+++ src/gameLayer/worldGenerator.cpp
@@ -8,22 +8,26 @@ void generateWorld(GameMap& gameMap, int seed) {
 
     int dirtHeight = height / 4; // Example: Half the height is dirt
 
-    std::ranlux24_base rng(seed); // Random number generator
+    std::ranlux24_base rng(seed++); // Random number generator
+
+    auto dirtNoise = FastNoise::New<FastNoise::Perlin>();
+    auto stoneNoise = FastNoise::New<FastNoise::Perlin>();
+
     int previousHeight = dirtHeight; // Start with the initial dirt height
 
     // Example: Fill the bottom half with dirt and the top half with air
     for (int x = 0; x < width; ++x) {
 
-        int offset = getRandomInt(rng, -1, 1); // Random offset for terrain variation
-        int adjustedDirtHeight = previousHeight + offset;
-        previousHeight = adjustedDirtHeight;
+        float fDirtHeight = dirtNoise->GenSingle2D(x, 0, seed);
+        int dirtHeight = fDirtHeight * 20 + 20;
 
-        int stoneHeight = adjustedDirtHeight + 10;
+        float fStoneHeight = dirtNoise->GenSingle2D(x, 0, seed+1);
+        int stoneHeight = fStoneHeight * 20 + 30;
 
         for (int y = 0; y < height; ++y) {
-            if (y == adjustedDirtHeight) {
+            if (y == dirtHeight) {
                 gameMap.getBlock(x, y).id = Block::grassBlock; // Grass block at the surface
-            } else if (y > adjustedDirtHeight && y <= stoneHeight) {
+            } else if (y > dirtHeight && y <= stoneHeight) {
                 gameMap.getBlock(x, y).id = Block::dirt;
             } else if (y > stoneHeight) {
                 gameMap.getBlock(x, y).id = Block::stone;
